dc0cd28e54309caf6f1c152b75301c83ed383f53
hlondonomoreno <hernan.l@serviciosdeintegracion.com>
2025-07-05 11:32:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const { createCanvas, loadImage } = require('canvas');
+const { createCanvas, loadImage } = require('@napi-rs/canvas');
 const { getDominantColor, mixColors } = require('./utils/colors');
 
 const app = express();
